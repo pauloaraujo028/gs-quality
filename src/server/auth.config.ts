2473e00542e0105c61ae0858873fee6678e8e82f
Paulo Araujo <pauloaraujo.phz@gmail.com>
2026-02-03 16:04:59 -0300

    feat: configuração prisma e authjs

---
+++ src/server/auth.config.ts
@@ -0,0 +1,33 @@
+import { getUserByEmail } from "@/data/user";
+import { loginSchema } from "@/features/auth/schemas";
+import bcrypt from "bcryptjs";
+import type { NextAuthConfig } from "next-auth";
+import Credentials from "next-auth/providers/credentials";
+
+export default {
+  providers: [
+    Credentials({
+      credentials: {
+        email: {},
+        password: {},
+      },
+      authorize: async (credentials) => {
+        const validatedFields = loginSchema.safeParse(credentials);
+
+        if (validatedFields.success) {
+          const { email, password } = validatedFields.data;
+
+          const user = await getUserByEmail(email);
+
+          if (!user || !user.password) return null;
+
+          const passwordMatch = await bcrypt.compare(password, user.password);
+
+          if (passwordMatch) return user;
+        }
+
+        return null;
+      },
+    }),
+  ],
+} satisfies NextAuthConfig;
---
+++ src/server/auth.ts
@@ -0,0 +1,47 @@
+import { getUserById } from "@/data/user";
+import { db } from "@/lib/prisma";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { UserRole } from "@prisma/client";
+import NextAuth from "next-auth";
+import authConfig from "./auth.config";
+
+export const { auth, handlers, signIn, signOut } = NextAuth({
+  callbacks: {
+    async session({ session, token }) {
+      if (token.sub && session.user) {
+        session.user.id = token.sub;
+      }
+
+      if (token.role && session.user) {
+        session.user.role = token.role as UserRole;
+      }
+
+      if (session.user) {
+        session.user.name = token.name;
+        session.user.email = token.email ?? "";
+      }
+
+      return session;
+    },
+    async jwt({ token }) {
+      if (!token.sub) return token;
+
+      const existingUser = await getUserById(token.sub);
+
+      if (!existingUser) return token;
+
+      token.name = existingUser.firstName;
+      token.email = existingUser.email;
+      token.role = existingUser.role;
+
+      return token;
+    },
+  },
+  pages: {
+    signIn: "/auth/login",
+    error: "/auth/error",
+  },
+  adapter: PrismaAdapter(db),
+  session: { strategy: "jwt" },
+  ...authConfig,
+});
